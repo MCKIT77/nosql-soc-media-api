b0e434f59e9fa82df50d67379304b577e360a112
Jacob McKitrick <jakemckit@gmail.com>
2023-09-11 19:40:40 -0400

    added utils data and seed

---
+++ utils/data.js
@@ -0,0 +1,38 @@
+const names = [
+    'John',
+    'Alice',
+    'Bob',
+    'Ella',
+    'David',
+    'Sophia',
+    'Liam',
+    'Olivia',
+    'Emily',
+    'Michael',
+    'Emma',
+    'James',
+    // Add more names as needed
+];
+
+const thoughtTexts = [
+    'What a beautiful day!',
+    'Just had a delicious meal.',
+    'Excited for the weekend!',
+    'Coding is fun!',
+    'Traveling to a new city.',
+    'Listening to great music.',
+    // Add more thought texts as needed
+];
+
+const reactions = [
+    { reactionBody: 'Like', username: 'User1' },
+    { reactionBody: 'Love', username: 'User2' },
+    { reactionBody: 'Haha', username: 'User3' },
+    // Add more reactions as needed
+];
+
+module.exports = {
+    names,
+    thoughtTexts,
+    reactions,
+};
